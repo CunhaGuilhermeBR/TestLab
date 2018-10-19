3a28c1064fb8b2b318cec109d2ff979008d910c4
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2018-10-19 09:39:44 -0300

    Create Aplicacao.java

---
+++ Aplicacao.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Aplicacao {
+   public static void main(String[] args){
+   CFila F1 = new CFila();
+   CFila F2 = new CFila();
+   int escolha =0;
+   Scanner ler = new Scanner(System.in);
+   do{
+       System.out.println("1 - Cadastrar morador");
+       System.out.println("2 - Imprimir lista de moradores cadastrados");
+       System.out.println("3 - Imprimir fila de espera");
+       System.out.println("4 - Pesquisar morador");
+       System.out.println("5 - Desistência/Exclusão");
+       System.out.println("6 - Sorteio");
+       System.out.println("7 - Parâmetros");
+       System.out.println("8 - Sair");
+       escolha=ler.nextInt();
+   } while (escolha !=8);
+   
+   }
+}
