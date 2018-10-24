695e2eae96bc83b15e4f6ee610cce81147c55da6
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2018-10-24 15:33:59 -0300

    Update Aplicacao.java

--- Aplicacao.java
+++ Aplicacao.java
@@ -1,9 +1,10 @@
-import javax.swing.Scanner;
+import java.util.Scanner;
 
 public class Aplicacao {
    public static void main(String[] args){
-   CFila F1 = new CFila();
-   CFila F2 = new CFila();
+   Scanner ler = new Scanner(System.in);
+  
+   Programa p = new Programa();
    int escolha =0;
    
    do{
@@ -16,7 +17,26 @@ public class Aplicacao {
        System.out.println("7 - Parâmetros");
        System.out.println("8 - Sair");
        escolha=ler.nextInt();
+       switch (escolha) {
+       case 1:  p.cadastrarMorador();
+       break;
+       case 2:  p.imprimirCadastrados();
+       break;
+       case 3:  p.imprimirEspera();
+       break;
+       case 4:  p.pesquisarMorador();
+       break;
+       case 5:  p.excluirMorador();
+       break;
+       case 6:  p.sorteio();
+       break;
+       case 7:  p.parametros();
+       break;
+       default: System.out.println("Opção inválida");
+       break;
+       }
+       
    } while (escolha !=8);
-   
+   ler.close();
    }
 }
