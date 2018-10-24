3b4956fdab300183ee4d25625b6fd04489035690
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2018-10-24 15:37:31 -0300

    Create Morador.java

---
+++ Morador.java
@@ -0,0 +1,14 @@
+public class Morador {
+      public int cpf, dependentes,tel;
+      public String nome, endereço;
+      public double renda;
+      
+      public Morador(int cpf, int dependentes, int tel, String nome, String endereço, double renda) {
+		this.cpf = cpf;
+		this.dependentes = dependentes;
+		this.tel = tel;
+		this.nome = nome;
+		this.endereço = endereço;
+		this.renda = renda;
+	}
+}
